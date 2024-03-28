92da5eea0dab9f2cc1769cf7ef4208ba21a0b40c
Kim Hochstedler Webb <52170271+kimberlywebb@users.noreply.github.com>
2024-03-28 09:46:37 -0400

    publish pittpalette

---
+++ R/pitt_palette.R
@@ -0,0 +1,95 @@
+# Code for `pittpalette` R package.
+# Code is adopted from Cianna Bedford-Peterson's `inauguration` R package.
+# Source code: https://github.com/ciannabp/inauguration
+# Please find the copyright description from Cianna Bedford-Peterson's R package at the end of this file.
+
+
+#' List of color palettes.
+#'
+#' Use \code{\link{render_pitt_palette}} to construct palettes of desired length.
+#'
+#' @export
+pitt_palettes <- list(
+  primary_contemporary = c("#003594", "#FFB81C"),
+  primary_legacy = c("#141B4D", "#00205B", "#B58500"),
+  primary = c("#003594", "#FFB81C", "#141B4D", "#00205B", "#B58500"),
+  secondary = c("black", "white", "#75787B", "#97999B", "#C8C9C7"),
+  accent_rich = c("#7C6992", "#7E5475", "#008264", "#DC582A", "#0081A6"),
+  accent_vibrant = c("#EDE939", "#71C5E8", "#E87722", "#A4D65E", "#DBC8B6"),
+  accent = c("#7C6992", "#7E5475", "#008264", "#DC582A", "#0081A6",
+             "#EDE939", "#71C5E8", "#E87722", "#A4D65E", "#DBC8B6")
+)
+
+#' Pitt palette rendering
+#'
+#' @param n Number of colors desired. Most palettes have 3-6 colors. Most color
+#'   schemes are derived from \href{https://www.brand.pitt.edu/visual-identity/color-palettes-0}.
+#'   If omitted, uses all colors.
+#' @param name Name of color palette. Choices are:
+#'   \code{"primary_contemporary"}, \code{"primary_legacy"}, \code{"secondary"},
+#'   \code{"accent_rich"}, and \code{"accent_vibrant"}.
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+#' @return A vector of colors.
+#' @export
+#' @keywords colors
+#' @examples
+#' render_carnegie_colors("classic")
+#' render_carnegie_colors("secondary", 5)
+render_pitt_palette <- function(name, n) {
+
+  pal <- pitt_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (n > length(pal)) {
+    stop("Number of colors requested is larger than the chosen palette can provide.")
+  }
+
+  out <- pal[1:n]
+
+  structure(out, class = "palette", name = name)
+}
+
+
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, col = "#32373D")
+}
+
+
+# MIT License
+
+# Copyright (c) 2021 Cianna Bedford-Petersen
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
---
+++ README.md
@@ -0,0 +1,38 @@
+# pittpalette
+Contains code for R package with R color palettes inspired by Carnegie Mellon University.
+Code adopted from the lovely `inauguration` R package by Cianna Bedford-Petersen, available [here](https://github.com/ciannabp/inauguration). 
+
+# R package installation
+Install the  `pittpalette` R package using the following code:
+``` r
+#install.packages("devtools")
+devtools::install_github("kimberlywebb/pittpalette", ref="main")
+library(pittpalette)
+```
+
+# Using the carnegiecolors package
+Use the `names` function to view the available color palettes.
+```r
+names(pitt_palettes)
+# [1] "primary_contemporary" "primary_legacy"    "secondary"       "accent_rich"         
+# [5] "accent_vibrant" 
+```
+
+Call the palette using the `render_pitt_palette` function
+```r
+render_pitt_palette("primary")
+```
+![](classic.png)
+
+View a subset of the accent color scheme
+```r
+render_pitt_palette("accent", 5)
+```
+![](accent.png)
+
+Use colors from `carnegie_palettes` in your graphs
+```r
+set.seed(1)
+hist(rnorm(n = 100, mean = 0, sd = 1), col = pitt_palettes[["primary_contemporary"]][1])
+```
+![](pittsogram.png)
